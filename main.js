0aa50ebd434985ca44e0805969bf8718cd590cfa
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-09-13 16:18:15 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("game");
+
+canvas.height = window.height;
+canvas.width = window.width;
+
+const data = [
+	["a", 100],
+  ["b", 100],
+  ["c", 100],
+  ["d", 100],
+  ["e", 100],
+]
