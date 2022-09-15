1dc788a521042c1294a256b251cb13987123c551
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-09-15 16:15:49 -0400

    initial push

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const canvas = document.getElementById("game");
+const canvas = document.getElementById("graph");
+let ctx = canvas.getContext("2d")
 
-canvas.height = window.height;
-canvas.width = window.width;
+H = canvas.height = innerHeight;
+W = canvas.width = innerWidth;
 
 const data = [
 	["a", 100],
@@ -10,3 +11,47 @@ const data = [
   ["d", 100],
   ["e", 100],
 ]
+
+const colors = {
+  0: "#f00",
+  1: "#0f0",
+  2: "#00f",
+  3: "#ff0",
+  4: "#f0f",
+  5: "#0ff"
+}
+
+let total = 0;
+data.forEach(x => total += x[1])
+
+const r = 200
+const margin = 10
+const time = 60
+const cw = false
+
+function animate() {
+  ctx.clearRect(0, 0, W, H)
+  ctx.save()
+
+
+  let a = 0
+  ctx.translate(r+margin, r+margin)
+
+  let curr = 0
+  data.forEach((x, i) => {
+    ctx.fillStyle = colors[i] ? colors[i]: `hsl(${Math.floor(Math.random()*360)}, 100, 50)`;
+
+    let arc = (x[1]/total)* 2*Math.PI
+    ctx.beginPath()
+    ctx.arc(0, 0, r, a, arc, cw)
+    ctx.lineTo(0, 0)
+    ctx.fill()
+    ctx.closePath()
+    a += arc
+  })
+
+  ctx.restore()
+  curr ++
+  
+  requestAnimationFrame(animate)
+} animate()
