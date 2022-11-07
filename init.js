5b75478bff4f1dbbcec5e986f919921793dac651
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-11-07 09:59:09 -0500

    grapher works
    must add clockwise or counter-clockwise, and add switch to modify
    sorting

--- init.js
+++ init.js
@@ -1,22 +1,54 @@
 const main = document.getElementById("main");
 
-const text = (s, type) => {
-  return `<div class="field field_v1">
-    <input id="${s}" class="field__input" placeholder=" " type="${type}" autocomplete="off">
+const text = (s, type, style, value) => {
+  return `<div class="field field_v1" id="${s}">
+    <input id="${s}" class="${style?"field__input":"color_picker"}" placeholder=" " type="${type}" autocomplete="off" value="${value}">
     <span class="field__label-wrap" aria-hidden="true">
-      <span class="field__label">${type} ${s}</span>
+      <span class="field__label">${style? type: ""} ${style? s: ""}</span>
     </span>
   </div>`
 }
 
+let data = [
+	["a", 100],
+  ["b", 100],
+  ["c", 100],
+  ["d", 100],
+  ["e", 100],
+]
+/*
+let data = [
+	["a", 100, "#1E3888"],
+  ["b", 100, "#E01A4F"],
+  ["c", 100, "#53B3CB"],
+  ["d", 100, "#93B5C6"],
+  ["e", 100, "#FF8C42"],
+]
+*/
 
+const colors = {
+  0: "#1E3888",
+  1: "#E01A4F",
+  2: "#53B3CB",
+  3: "#93B5C6",
+  4: "#FF8C42",
+  5: "#688E26"
+}
 
-for (let i = 0; i < 5; i++) {
-  const newDiv = document.createElement("div")
-  newDiv.innerHTML += text(i+1, "text")
-  newDiv.innerHTML += text(i+1, "number")
+let info = JSON.parse(localStorage.getItem("data"))
+info = info? info: data;
+let i = 0
+while (i < (info.length>5?info.length:5)) {
+  addRow(i, info[i])
+  i++
+}
+
+function addRow(i, data) {
+  const newDiv = document.createElement(`div`)
+  newDiv.setAttribute("id", `box${i}`)
+  newDiv.innerHTML += text(i, "text", true, data[0])
+  newDiv.innerHTML += text(i, "number", true, data[1])
+  newDiv.innerHTML += text(i, "color", false, data[2])
 
   main.appendChild(newDiv)
 }
-
-
--- main.js
+++ main.js
@@ -1,39 +1,22 @@
 const canvas = document.getElementById("graph");
 let ctx = canvas.getContext("2d")
 
-H = canvas.height = innerHeight;
-W = canvas.width = innerWidth;
+const W = canvas.height = canvas.width = innerHeight*.5;
 
-const bgc = "darkturquoise"
+const ta = document.getElementsByTagName("textarea")[0]
+ta.rows = W/16;
+ta.cols = W/8;
 
-let data = [
-	["a", 100],
-  ["b", 100],
-  ["c", 100],
-  ["d", 100],
-  ["e", 100],
-]
-
-const colors = {
-  0: "#1E3888",
-  1: "#E01A4F",
-  2: "#53B3CB",
-  3: "#93B5C6",
-  4: "#FF8C42",
-  5: "#688E26"
-}
-
-let total;
-setupData()
-
-const r = 200
 const margin = 10
+const r = W/2-margin
 const time = 45
 let curr = 0
 const cw = false
 
+setupData()
+
 function animate() {
-  ctx.clearRect(0, 0, W, H)
+  ctx.clearRect(0, 0, W, W)
   ctx.save()
 
   ctx.translate(r+margin, r+margin)
@@ -46,19 +29,12 @@ function animate() {
 
   if (curr <= time) {
     curr+=0.5
+    requestAnimationFrame(animate)
+    return
   }
-  
-  requestAnimationFrame(animate)
-} animate()
 
-document.addEventListener("keydown", handleKeyDown, false)
-
-function handleKeyDown(e) {
-  if (e.key == "ArrowRight") {
-    animate()
-  }
+  cancelAnimationFrame(animate)
 }
-
 
 
 function easeOut(x) {
@@ -92,9 +68,9 @@ function allTogether() {
 
 
 function doughnut() {
-  ctx.save()
-  ctx.lineWidth = 1
-  ctx.fillStyle = bgc
+  ctx.save();
+  ctx.lineWidth = 1;
+  ctx.globalCompositeOperation = 'destination-out'
   ctx.beginPath()
   ctx.arc(0, 0, r/2.5, 0, Math.PI*2, false)
   ctx.fill()
@@ -103,28 +79,154 @@ function doughnut() {
 }
 
 function setupData() {
-  total = 0;
-  data.map((x,i) => {
-    total += x[1]
-    const color = colors[i] ? 
-      colors[i]: `hsl(${Math.floor(Math.random()*360)}, 100%, 50%)`
-    x.push(color);
-    return x
-  })
-}
-
-
-let button = document.getElementsByTagName("button")[0]
-button.onclick = () => {
+  total = 0
   const texts = document.querySelectorAll("input[type='text']")
   const numbers = document.querySelectorAll("input[type='number']")
+  const colors = document.querySelectorAll("input[type='color']")
   const res = []
 
   for (let i = 0; i < texts.length; i++) {
-    res.push([texts[i].value, Number(numbers[i].value)])
+    res.push([texts[i].value, Number(numbers[i].value), colors[i].value])
   }
-
+  res.forEach(x => total += x[1])
+  res.sort((a, b) => a[1]<b[1]?1:-1)
+  localStorage.setItem("data", JSON.stringify(res))
   data = res
-  setupData()
+
+  ta.innerHTML = getText(JSON.stringify(data))
+
+  animate()
+}
+
+
+const button = document.getElementById("submit");
+button.onclick = () => {
   curr = 0
+  setupData()
+}
+
+function ranColor() {
+  return `#${ranVal()}${ranVal()}${ranVal()}`
+}
+
+function ranVal() {
+  let res = Math.floor(Math.random()*255)
+  if (res <= 16) {
+    res += 16;
+  }
+  return res.toString(16)
+}
+
+const empty = ['[name]', 100, ranColor()]
+const add = document.getElementById("add")
+add.onclick = () => {
+  empty[2] = ranColor()
+  addRow(++i, empty)
+}
+
+const re = document.getElementById("remove")
+re.onclick = () => {
+  remove()
+}
+
+function remove() {
+  const el = document.querySelectorAll("input[type='text']")
+  const id = el[el.length-1].id
+  const div = document.querySelector(`div[id="box${id}"]`)
+  div.remove()
+}
+
+function getText(data) {
+  const script = `
+    setupData()
+
+    function animate() {
+      ctx.clearRect(0, 0, W, H)
+      ctx.save()
+
+      ctx.translate(r+margin, r+margin)
+      ctx.rotate(-90 * Math.PI/180)
+
+      allTogether()
+
+      doughnut()
+      ctx.restore()
+
+      if (curr <= time) {
+        curr+=0.5
+        requestAnimationFrame(animate)
+        return
+      }
+
+      cancelAnimationFrame(animate)
+    }
+
+    function allTogether() {
+      let a = 0
+
+      data.forEach(x => {  
+        ctx.fillStyle = x[2];
+        ctx.lineWidth = 5
+        let change = easeInOut(curr/(time))
+        let arc = (x[1]/total)* change * 2*Math.PI
+        ctx.rotate(a * (cw?-1:1))
+        ctx.beginPath()
+        ctx.arc(0, 0, r, 0, arc * (cw?-1:1), cw)
+        ctx.stroke()
+        ctx.lineTo(0, 0)
+        ctx.fill()
+        ctx.rotate(-a * (cw?-1:1))
+        ctx.closePath()
+        a += arc
+      })
+
+    }
+
+    function doughnut() {
+      ctx.save();
+      ctx.lineWidth = 1;
+      ctx.globalCompositeOperation = 'destination-out'
+      ctx.beginPath()
+      ctx.arc(0, 0, r/2.5, 0, Math.PI*2, false)
+      ctx.fill()
+      ctx.closePath()
+      ctx.restore()
+    }
+
+    function setupData() {
+      total = 0
+      data.forEach(x => total += x[1])
+      data.sort((a, b) => a[1]<(sort*b[1])?1:-1)
+
+      animate()
+    }
+
+    function easeInOut(x) {
+      return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+    }
+  `
+
+  return (`
+    <body>
+      <canvas id="canvas"></canvas>
+      <script>
+        const canvas = document.getElementById("canvas");
+        let ctx = canvas.getContext("2d");
+
+        const W = canvas.width = innerWidth;
+        const H = canvas.height = innerHeight
+
+        const margin = 10
+        const r = Math.min(H, W)/2-margin
+        const time = 45
+        let curr = 0
+        let cw = false;
+        let sort = 1;
+
+        const data = ${data}
+
+        ${script}
+      </script>
+    </body>
+  `)
 }
